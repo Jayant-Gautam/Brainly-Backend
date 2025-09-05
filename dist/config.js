4ea4a11183ca4642aadd4a097a1fb3b0454017e1
Jayant-Gautam <jayant10449@gmail.com>
2025-09-05 19:42:02 +0530

    Remove dist folder from repo, keep locally

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const constants = {
-    mongoURL: "mongodb+srv://Jayant:ConnectDB@clustor1.z9s37qq.mongodb.net/brainly",
-    JWT_SECRET: "secret",
-};
-exports.default = constants;
